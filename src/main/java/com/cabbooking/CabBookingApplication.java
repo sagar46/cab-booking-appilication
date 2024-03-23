bfa128b6661731996e60179dc31a88abc6f975cd
sagar46 <sagar.kr1910@gmail.com>
2024-03-23 22:33:05 +0530

    feature/find-ride: find ride api completed

--- src/main/java/com/cabbooking/CabBookingApplication.java
+++ src/main/java/com/cabbooking/CabBookingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CabBookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CabBookingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CabBookingApplication.class, args);
+    }
 
 }
